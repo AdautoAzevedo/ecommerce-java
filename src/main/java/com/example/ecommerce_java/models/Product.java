a7f3e701f331f765b788182fe0cfe0ead19563c0
AdautoAzevedo <adautofreitasdeazevedo@gmail.com>
2024-09-24 10:39:31 -0300

    Add products model

---
+++ src/main/java/com/example/ecommerce_java/models/Product.java
@@ -0,0 +1,27 @@
+package com.example.ecommerce_java.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Table(name = "products")
+@Getter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long id;
+
+    @Column(name = "product_name")
+    private String name;
+
+    @Column(name = "product_price")
+    private double price;
+    
+
+}
