5bc38b0a79321196f4dff79ac0e4d8b984a728bb
AdautoAzevedo <adautofreitasdeazevedo@gmail.com>
2024-10-18 15:03:11 -0300

    Added Product service and controller

---
+++ src/main/java/com/example/ecommerce_java/controllers/ProductController.java
@@ -0,0 +1,56 @@
+package com.example.ecommerce_java.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ecommerce_java.models.Product;
+import com.example.ecommerce_java.services.ProductService;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = service.getAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = service.getProductById(id);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Product> saveProduct(@RequestBody Product product) {
+        Product savedProduct = service.createProduct(product);
+        return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long productId, @RequestBody Product correctedProduct) {
+        correctedProduct.setId(productId);
+        Product product = service.updateProduct(correctedProduct);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long productId) {
+        service.deleteProduct(productId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ src/main/java/com/example/ecommerce_java/services/ProductService.java
@@ -0,0 +1,39 @@
+package com.example.ecommerce_java.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ecommerce_java.models.Product;
+import com.example.ecommerce_java.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository repository;
+
+    public Product getProductById(Long productId) {
+        return repository.findById(productId).get();
+    }
+
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+
+    public Product createProduct(Product product) {
+        return repository.save(product);
+    }
+
+    public Product updateProduct(Product correctedProduct) {
+        Product product = repository.findById(correctedProduct.getId()).get();
+        product.setName(correctedProduct.getName());
+        product.setPrice(correctedProduct.getPrice());
+        product.setCategory(correctedProduct.getCategory());
+        return repository.save(product);
+    }
+
+    public void deleteProduct(Long productId) {
+        repository.deleteById(productId);
+    }
+}
