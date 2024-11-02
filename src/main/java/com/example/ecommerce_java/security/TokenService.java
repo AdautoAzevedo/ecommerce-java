d1ccc1390d88c6356a7a7efd7d2c1ae3804713f2
AdautoAzevedo <adautofreitasdeazevedo@gmail.com>
2024-11-02 14:22:54 -0300

    Add Token Service

--- src/main/java/com/example/ecommerce_java/security/TokenService.java
+++ src/main/java/com/example/ecommerce_java/security/TokenService.java
@@ -1,8 +1,15 @@
 package com.example.ecommerce_java.security;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 import com.example.ecommerce_java.models.UserPrincipal;
 
 @Service
@@ -11,6 +18,36 @@ public class TokenService {
     private String secret;
 
     public String generateToken(UserPrincipal userPrincipal) {
-        
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                .withIssuer("ecommerce-api")
+                .withSubject(userPrincipal.getUsername())
+                .withExpiresAt(genExpirationDate())
+                .sign(algorithm);
+            return token;
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error wi=hile genenrating token: ", exception);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("ecommerce-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (Exception e) {
+           System.out.println("JWT verification exception: " + e);
+           return "JWT verification exception: " + e;
+        }
+    }
+
+
+    private Instant genExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 }
